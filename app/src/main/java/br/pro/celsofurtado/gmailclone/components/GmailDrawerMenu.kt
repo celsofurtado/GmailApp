2bb55b9782dded218ac2e29eabb5adfb2d444750
Celso Furtado <celsofurtado@terra.com.br>
2022-11-11 15:23:43 -0300

    Aula 91 concluída

--- app/src/main/java/br/pro/celsofurtado/gmailclone/components/GmailDrawerMenu.kt
+++ app/src/main/java/br/pro/celsofurtado/gmailclone/components/GmailDrawerMenu.kt
@@ -1,29 +1,111 @@
 package br.pro.celsofurtado.gmailclone.components
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.pro.celsofurtado.gmailclone.DrawerMenuData
 
 @Composable
-fun GmailDrawerMenu() {
+fun GmailDrawerMenu(scrollState: ScrollState) {
 
-    val menuList = listOf(DrawerMenuData.AllInboxes)
+    val menuList = listOf(
+        DrawerMenuData.Divider,
+        DrawerMenuData.AllInboxes,
+        DrawerMenuData.Divider,
+        DrawerMenuData.Primary,
+        DrawerMenuData.Social,
+        DrawerMenuData.Promotions,
+        DrawerMenuData.HeaderOne,
+        DrawerMenuData.Starred,
+        DrawerMenuData.Snoozed,
+        DrawerMenuData.Important,
+        DrawerMenuData.Sent,
+        DrawerMenuData.Schedule,
+        DrawerMenuData.Outbox,
+        DrawerMenuData.Draft,
+        DrawerMenuData.AllMail,
+        DrawerMenuData.HeaderTwo,
+        DrawerMenuData.Calendar,
+        DrawerMenuData.Contacts,
+        DrawerMenuData.Divider,
+        DrawerMenuData.Setting,
+        DrawerMenuData.Help
+    )
 
-    Column() {
+    Column(Modifier.verticalScroll(scrollState)) {
         Text(
             text = "Gmail",
             color = Color.Red,
-            modifier = Modifier.padding(start = 20.dp, top = 20.dp),
+            modifier = Modifier
+                .padding(start = 20.dp, top = 20.dp),
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold
         )
+
+        menuList.forEach { item ->
+
+            when {
+                item.isDivider -> {
+                    Divider(
+                        modifier = Modifier
+                            .padding(bottom = 20.dp, top = 20.dp)
+                            .border(20.dp, Color.Gray, RectangleShape)
+                    )
+                }
+                item.isHeader -> {
+                    Text(
+                        text = item.title!!,
+                        fontWeight = FontWeight.Light,
+                        modifier = Modifier
+                            .padding(start = 20.dp, top = 20.dp, bottom = 20.dp)
+                    )
+                } else -> {
+                    MailDrawerItem(item = item)
+                }
+            }
+
+        }
+
     }
 }
+
+@Composable
+fun MailDrawerItem(item: DrawerMenuData) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp)
+            .padding(top = 16.dp)
+    ) {
+        Image(
+            imageVector = item.icon!!,
+            contentDescription = item.title!!,
+            modifier = Modifier.weight(0.5f)
+        )
+        Text(
+            text = item.title,
+            modifier = Modifier.weight(2.0f)
+        )
+    }
+}
+
+
+
+
+
+
+
